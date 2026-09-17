b599775964c7c5d99f4a8f3a45d4c5e54439a782
mohamed <mohamed@deltauniv.edu.eg>
2026-09-18 01:28:58 +0300

    Remove AI414 B_Cyber lecture after successful registration, keep B4_Cyber section

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ EXCLUDED_COURSES = [normalize_code(c) for c in raw_excluded.split(',') if c.stri
 # If a course is in TARGET_GROUPS, ONLY these groups are monitored/alerted.
 # Other non-excluded courses will monitor all groups as usual.
 DEFAULT_TARGET_GROUPS = {
-    'AI414': ['B_Cyber', 'B4_Cyber'],
+    'AI414': ['B4_Cyber'],
     'SEC413': ['A', 'B4'],
     'SECE43': ['B', 'B3'],
     'SEC415': ['B1'],
