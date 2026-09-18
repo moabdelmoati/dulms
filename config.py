bd69076718c366987f8a4ddf8a1d445ab453ab85
mohamed <mohamed@deltauniv.edu.eg>
2026-09-18 12:54:07 +0300

    Keep SECE43 section B3 only, remove lecture B

--- config.py
+++ config.py
@@ -25,7 +25,7 @@ ALERT_REPEAT_COUNT = int(os.getenv('ALERT_REPEAT_COUNT', '20'))
 ALERT_INTERVAL_SECONDS = int(os.getenv('ALERT_INTERVAL_SECONDS', '3'))
 
 # Courses to completely exclude from checking and alerting
-raw_excluded = os.getenv('EXCLUDED_COURSES', 'SEC411,SEC412,GEN403,SECE43')
+raw_excluded = os.getenv('EXCLUDED_COURSES', 'SEC411,SEC412,GEN403')
 EXCLUDED_COURSES = [normalize_code(c) for c in raw_excluded.split(',') if c.strip()]
 
 # Specific target groups to monitor per course (normalized code -> list of group names)
@@ -34,6 +34,7 @@ EXCLUDED_COURSES = [normalize_code(c) for c in raw_excluded.split(',') if c.stri
 DEFAULT_TARGET_GROUPS = {
     'AI414': ['B4_Cyber'],
     'SEC413': ['A', 'B4'],
+    'SECE43': ['B3'],
     'SEC415': ['B1'],
 }
 
